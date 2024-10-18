1098fd723fa66618bb53a4beb803c96b2db147a1
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-18 17:26:17 -0600

    add method for posting data to the DB withing the API hook

--- fe/app/hooks/useContentApi.ts
+++ fe/app/hooks/useContentApi.ts
@@ -9,10 +9,23 @@ const fetchAllContent = async () => {
 	return result;
 };
 
+const addContent = async (data: Omit<ContentRecord, '_id'>) => {
+	const result = await fetch(`http://127.0.01:3000/data`, {
+		method: 'POST',
+		mode: 'no-cors',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	return result;
+};
+
 export const useContentApi = () => {
 	const [state, setState] = useState<ContentRecord[] | null>(null);
 	const [loadData, setLoadData] = useState(true);
 	const [isLoading, setIsLoading] = useState(false);
+	const [isModifyingData, setIsModifyingData] = useState(false);
 	const [error, setError] = useState<Error | null>(null);
 
 	useEffect(() => {
@@ -25,23 +38,55 @@ export const useContentApi = () => {
 			try {
 				const data = await res.json() as ContentRecord[];
 				setState(data);
+				console.log(data);
 			} catch (error) {
 				if (!!error && error instanceof Error) {
 					setError(error);
 				}
 			}
 			setIsLoading(false);
+		}).catch(error => {
+			if (!!error && error instanceof Error) {
+				setError(error);
+			}
+			setIsLoading(false);
 		});
 	});
 
-	const updateData = useCallback(() => {
+	const fetchData = useCallback(() => {
 		setLoadData(true);
 	}, [setLoadData])
+
+	const addData = useCallback(async (data: Omit<ContentRecord, '_id'>) => {
+		setIsModifyingData(true);
+		addContent(data).then(async res => {
+			try {
+				const isOk = res.ok && await res.json();
+				if (isOk === true) {
+					console.log("Content added: ", data);
+					return true;
+				}
+			} catch (error) {
+				if (!!error && error instanceof Error) {
+					setError(error);
+				}
+			}
+			setIsModifyingData(false);
+			setLoadData(true);
+		}).catch(error => {
+			if (!!error && error instanceof Error) {
+				setError(error);
+			}
+			setIsModifyingData(false);
+		});
+	}, [setState, setLoadData, setIsModifyingData, setError]);
 
 	return {
 		data: state,
 		isLoading,
+		isModifyingData,
 		error,
-		updateData,
+		fetchData,
+		addData,
 	};
 };
