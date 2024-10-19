10de0e62f1ac9d6c92cafe9137eb8b2bf1618452
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-19 15:24:39 -0600

    apply NextUI tailwind plugin

--- fe/tailwind.config.ts
+++ fe/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(tabs|table).js",
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,6 +18,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui()
+  ],
 }
 export default config
