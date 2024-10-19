833e61a1634b45254bafd27110d69dcbdb033f08
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-19 15:23:43 -0600

    update delete endpoint to use query params

--- be/src/app.controller.ts
+++ be/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Req, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Delete, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DataDto } from 'src/data.dto';
 
@@ -13,11 +13,11 @@ export class AppController {
 
   @Post()
   create(@Body() body: DataDto) {
-    return this.appService.create(body);
-  }
+      return this.appService.create(body);
+    }
 
-  @Delete(':id')
-  delete(@Param() id: string) {
-    return this.appService.delete(id);
+  @Delete()
+  delete (@Query('id') id: string) {
+      return this.appService.delete(id);
+    }
   }
-}
