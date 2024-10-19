56d28005b78fed2b73d9c6917be21e1fca6dac86
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-19 15:25:07 -0600

    factor out list item from data list

--- fe/app/components/dataList.tsx
+++ fe/app/components/dataList.tsx
@@ -1,17 +1,34 @@
+import { Button, Card, CardHeader, CardBody } from "@nextui-org/react";
+
 export type ContentRecord = {
 	_id?: string;
 	data: string;
 }
 
-export const DataList = (props: { items: ContentRecord[] }) => {
+const ListItem = (props: { item: ContentRecord, deleteItem: (id: string) =>  void }) => {
+	const { item, deleteItem } = props;
+	const { _id, data } = item;
 	return (
-		<div className="flex flex-col">
-			{props.items.map((item, index) => {
-				const { _id, data } = item;
-				return (<div key={_id} className="flex flex-row gap-4">
-					<div>{data}</div>
-				</div>)
-			})}
+		<div key={_id} className="flex gap-4">
+			<Card className="w-full">
+				<CardBody className="flex flex-row justify-between">
+					<div className="flex flex-row justify-start w-full">
+						{data}
+					</div>
+					<div className="flex flex-row justify-end">
+						<Button size="sm" color="secondary" onClick={() => _id && deleteItem(_id)}>Delete</Button>
+					</div>
+				</CardBody>
+			</Card>
+		</div>
+	)
+};
+
+export const DataList = (props: { items: ContentRecord[] | null, deleteItem: (id: string) =>  void }) => {
+	const { items, deleteItem } = props;
+	return (
+		<div className="flex flex-col-reverse gap-4 w-full p-24">
+			{items && items.map((item) => <ListItem key={item._id} item={item} deleteItem={deleteItem} />)}
 		</div>
 	)
 }
