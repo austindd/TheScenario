b0374ad2711d388c9e11999f876981696c188acb
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-18 23:09:52 -0600

    add service method for uploading data; also use real data from DB

--- be/src/app.service.ts
+++ be/src/app.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { DataDao } from './data.dao';
+import { Data, DataDocument } from 'src/data.db';
+import { DataDto } from 'src/data.dto';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AppService {
 
   constructor(
-    private readonly dataDao: DataDao
+    private readonly dataDao: DataDao,
+    @InjectModel(Data.name, 'local') private dataModel: Model<DataDocument>
   ){}
 
 
   async getAll() {
-    return [{_id: '1', userName: 'TestUser', content: '123'}]
+    // return [{_id: '1', userName: 'TestUser', content: '123'}]
 
-    //return await this.dataDao.getAll()
+    return await this.dataDao.getAll()
+  }
+
+  async create(data: DataDto) {
+    // return {_id: '1', userName: 'TestUser', content: '123'}
+    const newData = new this.dataModel(data);
+    return await this.dataDao.create(newData)
   }
 }
