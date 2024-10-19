6a83fa41cf4d8787af48efba580ab60a592133eb
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-19 15:25:36 -0600

    cleanup commented code and update styles on form component

--- fe/app/components/form.tsx
+++ fe/app/components/form.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import { Input, Button } from '@nextui-org/react';
-import FormInputList from './formInputList';
 import { ContentRecord } from '@/app/components/dataList';
 
 type FormValues = Omit<ContentRecord, '_id'>;
@@ -19,28 +18,28 @@ const DataForm = (props: { onSubmit: (values: FormValues) => void }) => {
 
 	return (
 		<form onSubmit={formik.handleSubmit}>
-			<div className="flex flex-col gap-4">
-				<Input
-					name="data"
-					value={formik.values.data}
-					onChange={event => formik.handleChange('data')(event.target.value)}
-					label="Data"
-					placeholder="exampleUser42"
-					size="lg"
-					radius="md"
-				/>
-				{/* <Input
-					name="content"
-					value={formik.values.content}
-					onChange={event => formik.handleChange('content')(event.target.value)}
-					label="Content"
-					placeholder="ABC xyz"
-					size="lg"
-					radius="md"
-				/> */}
-			</div>
-			<div className="flex flex-col gap-4">
-				<Button radius='md' type="submit" size="lg">Submit</Button>
+			<div className="flex flex-col gap-8">
+				<div className="flex flex-col gap-4">
+					<Input
+						name="data"
+						value={formik.values.data}
+						onChange={event => formik.handleChange('data')(event.target.value)}
+						label="Data"
+						placeholder="exampleUser42"
+						size="lg"
+						radius="md"
+						color="primary"
+					/>
+				</div>
+				<div className="flex flex-col gap-4">
+					<Button
+						radius='md'
+						type="submit"
+						size="lg"
+					>
+						Submit
+					</Button>
+				</div>
 			</div>
 		</form>
 	);
