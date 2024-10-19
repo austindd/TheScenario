55256923e47965ed7325172b9566267fc666df92
Austin Davis <44079572+austindd@users.noreply.github.com>
2024-10-18 23:12:01 -0600

    add bodyParser explicitly; enable cors

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.enableCors();
 
   await app.listen(3000);
 }
